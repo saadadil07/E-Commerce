1dfad0be15addf70ee7e9f111705a6648b722ed4
Muhammad Saad Adil <saadadil321631@gmail.com>
2024-03-05 14:27:39 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+burger = document.querySelector('.burger')
+nav_bar = document.querySelector('nav')
+visibility = document.querySelector('.v-class')
+height = document.querySelector('.h-nav')
+
+burger.addEventListener('click', ()=>
+(
+    nav_bar.addEventListener('visibility') ;
+    nav_bar.addEventListener('height') ;
+))
